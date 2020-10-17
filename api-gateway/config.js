82b918dd804e50492049047a8e21ed2d39828c44
Wendy Leon <pixelmamma@gmail.com>
2020-10-17 00:03:10 -0400

    Adding 1.4 API Gateway Part I

---
+++ api-gateway/config.js
@@ -0,0 +1,13 @@
+/** 
+========================================
+; Title:  config.js
+; Author: Wendy Leon
+; Date:   10/16/20
+; Description:config.js
+;=======================================
+**/
+
+var config = {};
+config.web = {};
+config.web.port = process.env.PORT || '3000';
+module.exports = config;
